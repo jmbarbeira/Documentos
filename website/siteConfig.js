903ea61d76c03c4615a15979121b57c32f85f563
jmbarbeira <jbarbeira@gmail.com>
2020-03-13 17:43:47 +0100

    Configuracion

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Handpoint`,
+  copyright: `Copyright © ${new Date().getFullYear()} Handpoint2`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
